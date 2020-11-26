a05c0c3d729aa33e9a26339e6b19b264c9a023f5
Aditya Verma <aaadarths2357@gmail.com>
2020-11-26 23:49:38 +0530

    Added: code to launch bridgelabz site on clicking the bridgelabz logo using event binding technique

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import logo from './assets/BridgeLabz-2.jpg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+class App extends React.Component {
+    url='http://bridgelabz.com/'
+    constructor() {
+        super()
+        this.state = {
+            title: 'Hello from BridgeLabz'
+        }
+    }
+    onClick=($event) => {
+        console.log("save button is  clicked:",$event);
+        window.open(this.url,"_blank");
+    };
+    render() {
+        return ( 
+        <div>
+            <h1>{this.state.title}</h1>
+            <img src={logo} onClick={this.onClick} 
+            alt="The Bridgelabz Logo"/>
+            </div>
+        );
+    }       
 }
 
 export default App;
